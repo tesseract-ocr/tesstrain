1d3ea4afd7d6d97cd77d2754bf43b2034c169c5c
Shreeshrii <shreeshrii@gmail.com>
2019-09-03 22:56:39 +0530

    Create generate_wordstr_box.py

---
+++ generate_wordstr_box.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import io
+import argparse
+import unicodedata
+from PIL import Image
+
+#
+# command line arguments
+#
+arg_parser = argparse.ArgumentParser('''Creates tesseract WordStr box files for given (line) image text pairs''')
+
+# Text ground truth
+arg_parser.add_argument('-t', '--txt', nargs='?', metavar='TXT', help='Line text (GT)', required=True)
+
+# Image file
+arg_parser.add_argument('-i', '--image', nargs='?', metavar='IMAGE', help='Image file', required=True)
+
+args = arg_parser.parse_args()
+
+#
+# main
+#
+
+# load image
+with open(args.image, "rb") as f:
+    width, height = Image.open(f).size
+
+# load gt
+with io.open(args.txt, "r", encoding='utf-8') as f:
+    lines = f.read().strip().split('\n')
+
+# create WordStr line boxes for Indic & RTL
+for line in lines:
+    if line.strip():
+        print(u"%s %d %d %d %d 0 #%s" % ("WordStr", 0, 0, width, height, line))
+        print(u"%s %d %d %d %d 0" % ("\t", width, height, width+1, height+1))
