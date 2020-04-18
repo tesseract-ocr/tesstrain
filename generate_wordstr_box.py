c060e859180e1ac2f3c4e7eaf2ae708ce445dfd2
Shree <shreeshrii@gmail.com>
2020-04-18 10:13:20 +0000

    Remove trimming of images

--- generate_wordstr_box.py
+++ generate_wordstr_box.py
@@ -3,7 +3,7 @@
 import argparse
 import io
 import unicodedata
-from PIL import Image, ImageChops, ImageOps
+from PIL import Image
 
 #
 # command line arguments
@@ -22,20 +22,10 @@ args = arg_parser.parse_args()
 # main
 #
 
-def trim(im):
-    bg = Image.new(im.mode, im.size, im.getpixel((0,0)))
-    diff = ImageChops.difference(im, bg)
-    diff = ImageChops.add(diff, diff, 2.0, -100)
-    bbox = diff.getbbox()
-    if bbox:
-        return im.crop(bbox)
-
 # load image
 with open(args.image, "rb") as f:
-    cropped = trim(Image.open(f))
-    img_with_border = ImageOps.expand(cropped,border=1,fill='white')
-    width, height = img_with_border.size
-    img_with_border.save(args.image)
+    im = Image.open(f)
+    width, height = im.size
 
 # load gt
 with io.open(args.txt, "r", encoding='utf-8') as f:
