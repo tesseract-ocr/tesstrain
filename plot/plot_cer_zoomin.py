34956869820e9849c8657d4aa77df143d89cdc06
Shree <5095331+Shreeshrii@users.noreply.github.com5095331+Shreeshrii@users.noreply.github.com>
2020-11-09 04:13:33 +0000

    Add IASTplus, IAST_LAYER and santest_PLUS plots as examples

--- plot/plot_cer_zoomin.py
+++ plot/plot_cer_zoomin.py
@@ -5,15 +5,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-csvlinesstep=10
-maxCERtoDisplay=7
+csvlinesstep=50
+maxCERtoDisplay=11
 PlotTitle="Tesseract LSTM training and Evaluation Character Error Rates (-1 to " + str(maxCERtoDisplay) + "%)"
 
 dataframe = pd.read_csv("plot_cer.csv",sep='\t', encoding='utf-8')
-dataframe['TrainingIteration'] = dataframe['TrainingIteration'].fillna(-1)
+dataframe['TrainingIteration'] = dataframe['TrainingIteration'].fillna(-2)
 dataframe['TrainingIteration'] = dataframe['TrainingIteration'].astype(int)
 dataframe['TrainingIteration'] = dataframe['TrainingIteration'].astype(str)
-dataframe['TrainingIteration'] = dataframe['TrainingIteration'].replace('-1', np.nan)
+dataframe['TrainingIteration'] = dataframe['TrainingIteration'].replace('-2', np.nan)
 t = dataframe['TrainingIteration']
 tticks = dataframe['TrainingIteration'][::csvlinesstep]
 x = dataframe['LearningIteration']
@@ -22,13 +22,13 @@ y = dataframe.IterationCER
 c = dataframe.CheckpointCER
 e = dataframe.EvalCER
 
-def annot_min(boxcolor, x,y):
+def annot_min(boxcolor, xpos, ypos, x,y):
     xmin = x[np.argmin(y)]
     ymin = y.min()
-    boxtext= "Minimum CER = {:.3f}% at Learning Iteration = {:}" .format(ymin,xmin)
-    ax1.annotate(boxtext, xy=(xmin, ymin), xytext=(-250,10),
-            textcoords='offset points', 
-            bbox=dict(boxstyle='round,pad=0.1', fc=boxcolor, alpha=0.3))
+    boxtext= "{:.3f}% at Learning Iteration {:}" .format(ymin,xmin)
+    ax1.annotate(boxtext, xy=(xmin, ymin), xytext=(xpos,ypos), textcoords='offset points',
+            arrowprops=dict(shrinkA=0.05, shrinkB=1, fc=boxcolor, ec='white', connectionstyle="arc3"),
+            bbox=dict(boxstyle='round,pad=0.2', fc=boxcolor, alpha=0.3))
 
 fig = plt.figure(figsize=(11,8.5)) #size is in inches
 ax1 = fig.add_subplot()
@@ -36,16 +36,18 @@ ax1.set_ylim([-1,maxCERtoDisplay])
 ax1.set_xlabel('Learning Iterations')
 ax1.set_ylabel('Character Error Rate (%)')
 ax1.set_xticks(xticks)
-ax1.tick_params(axis='x', rotation=90, labelsize='small')
+ax1.tick_params(axis='x', rotation=45, labelsize='small')
 ax1.grid(True)
-ax1.scatter(x, c, c='gold', s=50, label='Best model checkpoint CER')
-annot_min('gold',x,c)
+ax1.scatter(x, c, c='gold', s=50, label='Best Model Checkpoints CER')
+ax1.plot(x, c, 'gold')
+annot_min('gold',-150,-30,x,c)
 
-ax1.scatter(x, y, s=5, c='teal', label='Training CER every 100 iterations')
+ax1.scatter(x, y, s=5, c='teal', label='CER every 100 Training Iterations')
 ax1.plot(x, y, 'teal')
 
 ax1.plot(x, e, 'magenta')
-ax1.scatter(x, e, c='magenta', s=11, label='Evaluation CER')
+ax1.scatter(x, e, c='magenta', s=50, label='Evaluation CER')
+annot_min('magenta',-150,60,x,e) #uncomment only when eval data exists
 
 plt.title(label=PlotTitle)
 plt.legend(loc='upper right')
@@ -54,7 +56,7 @@ ax2 = ax1.twiny() # ax1 and ax2 share y-axis
 ax2.set_xlabel("Training Iterations")
 ax2.set_xlim(ax1.get_xlim()) # ensure the independant x-axes now span the same range
 ax2.set_xticks(xticks) # copy over the locations of the x-ticks from Learning Iterations
-ax2.tick_params(axis='x', rotation=90, labelsize='small')
+ax2.tick_params(axis='x', rotation=45, labelsize='small')
 ax2.set_xticklabels(tticks) # But give value of Training Iterations
 
-plt.savefig("plot_cer_plus.png")
+plt.savefig("plot_cer_zoomin.png")
