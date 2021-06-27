3582f37b5c966076a0c7daed4f7bc95e6f89bcdc
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2021-06-28 01:45:54 +0200

    shuffle.py: allow applying in-place on files directly

--- shuffle.py
+++ shuffle.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
-# shuffle.py - shuffle the lines from stdin in pseudo random order
+# shuffle.py - shuffle lines in pseudo random order
 #
 # Usage:
-#       shuffle.py [SEED]
+#       shuffle.py [SEED [FILE]]
 #
 # Sort and shuffle the lines read from stdin in pseudo random order
 # and write them to stdout.
+#
+# If FILE is given, then apply to that in-place (instead of stdin and stdout).
 #
 # The optional SEED argument is used as a seed for the random generator.
 # A shuffled list can be reproduced by using the same seed again.
@@ -18,8 +20,13 @@ import sys
 if len(sys.argv) > 1:
     random.seed(sys.argv[1])
 
+if len(sys.argv) > 2:
+    fd0 = open(sys.argv[2], 'r')
+else:
+    fd0 = sys.stdin
+
 # Read lines from standard input.
-lines = sys.stdin.readlines()
+lines = fd0.readlines()
 
 # First sort the input lines (directory entries may come in undefined order).
 lines.sort()
@@ -27,5 +34,10 @@ lines.sort()
 # Then shuffle the lines.
 random.shuffle(lines)
 
+if len(sys.argv) > 2:
+    fd1 = open(sys.argv[2], 'w')
+else:
+    fd1 = sys.stdout
+
 # Write the shuffled lines to standard output.
-sys.stdout.writelines(lines)
+fd1.writelines(lines)
