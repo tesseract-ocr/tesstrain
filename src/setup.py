e91a31dc9926ea2dadff0117c3ff8d5a976d6aaf
stefan6419846 <96178532+stefan6419846@users.noreply.github.com>
2023-01-08 17:28:47 +0100

    update supported Python versions

--- src/setup.py
+++ src/setup.py
@@ -21,15 +21,15 @@ setuptools.setup(
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     keywords='Tesseract,tesseract-ocr,OCR,optical character recognition',
 
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     install_requires=[
         'tqdm',
     ],
