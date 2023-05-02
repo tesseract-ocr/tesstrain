tesseract-ocr/tesstrain

commit 397db54dd69febfcc240937814409bb3d23230bb

simplify recipe for box→lstmf

Makefile
@@ -238,18 +238,22 @@ $(ALL_LSTMF): $(ALL_FILES:%.gt.txt=%.lstmf)
 	$(file >$@) $(foreach F,$^,$(file >>$@,$F))
 	python3 shuffle.py $(RANDOM_SEED) "$@"
 
-%.lstmf: %.box
-	@if test -f "$*.png"; then \
-	  image="$*.png"; \
-	elif test -f "$*.bin.png"; then \
-	  image="$*.bin.png"; \
-	elif test -f "$*.nrm.png"; then \
-	  image="$*.nrm.png"; \
-	else \
-	  image="$*.tif"; \
-	fi; \
+%.lstmf: %.png %.box
 	set -x; \
-	tesseract "$${image}" $* --psm $(PSM) lstm.train
+	tesseract "$<" $* --psm $(PSM) lstm.train
+
+%.lstmf: %.bin.png %.box
+	set -x; \
+	tesseract "$<" $* --psm $(PSM) lstm.train
+
+%.lstmf: %.nrm.png %.box
+	set -x; \
+	tesseract "$<" $* --psm $(PSM) lstm.train
+
+%.lstmf: %.tif %.box
+	set -x; \
+	tesseract "$<" $* --psm $(PSM) lstm.train
+
 
 CHECKPOINT_FILES := $(wildcard $(OUTPUT_DIR)/checkpoints/$(MODEL_NAME)*.checkpoint)
 .PHONY: traineddata

commit 02d94fcb0682b9c78761fcea3bcd13a7ada74b47

Remove unnecessary `set -x` commands
They create confusing output when building the lstmf files
and also slow down that build.

Fixes: 397db54dd69febfc ("simplify recipe for box→lstmf")
Signed-off-by: Stefan Weil <sw@weilnetz.de>

Makefile
@@ -255,23 +255,18 @@ $(ALL_LSTMF): $(ALL_FILES:%.gt.txt=%.lstmf)
 
 .PRECIOUS: %.lstmf
 %.lstmf: %.png %.box
-	set -x; \
 	tesseract "$<" $* --psm $(PSM) lstm.train
 
 %.lstmf: %.bin.png %.box
-	set -x; \
 	tesseract "$<" $* --psm $(PSM) lstm.train
 
 %.lstmf: %.nrm.png %.box
-	set -x; \
 	tesseract "$<" $* --psm $(PSM) lstm.train
 
 %.lstmf: %.raw.png %.box
-	set -x; \
 	tesseract "$<" $* --psm $(PSM) lstm.train
 
 %.lstmf: %.tif %.box
-	set -x; \
 	tesseract "$<" $* --psm $(PSM) lstm.train
 
 CHECKPOINT_FILES := $(wildcard $(OUTPUT_DIR)/checkpoints/$(MODEL_NAME)*.checkpoint)